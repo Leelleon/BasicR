53a32a0442613ff0765e1227e3f53a8bae53d66d
Leelleon <leonardosantosbran@gmail.com>
2020-07-17 14:08:35 -0300

    Primeros dos HandOns

---
+++ Date and Time.R
@@ -0,0 +1,4 @@
+# Date and Time
+x <- "25-12-2016"
+date <- as.Date(x, "%d-%m-%Y")
+date
---
+++ Levels and Names.R
@@ -0,0 +1,8 @@
+#Level and Names
+
+my_vector <- c("yes", "no", "yes", "maybe")
+my_vector_factor <- factor(my_vector)
+my_vector_factor
+
+
+
